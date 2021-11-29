3d0e24fc8a235c0d17d735f0c226ddfc41a044cd
David Rendell <davidwrendell@gmail.com>
2021-11-29 14:05:58 +0000

    Setup Hello World React app

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+const HelloWorld = () => <h1>Hello, World</h1>
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById("root"),
+)
